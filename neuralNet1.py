deae8a2daaa132dd97631a1b100d240e15890421
hchiam <hchiam@gmail.com>
2016-08-17 13:38:19 -0400

    added commenting

--- neuralNet1.py
+++ neuralNet1.py
@@ -1,15 +1,21 @@
 # i=input, w=weight, g=guess, a=answer, e=error, d=direction to move g closer to a, s=sensitivity
-i1 = 0
-i2 = 1
-w1 = -1
-w2 = -1
-a = 1
-s = 0.1
-for iter in range(5):
-    g = i1*w1 + i2*w2
-    e = abs(a-g)
-    d = s*(a-g)/abs(a-g)
-    w1 = w1 + d
-    w2 = w2 + d
-    print g, e, w1, w2, d
-print g
+
+i1 = 0 # input 1
+i2 = 1 # input 2
+w1 = -1 # weight 1
+w2 = -1 # weight 2
+a = 1 # the correct "answer"
+
+s = 0.1 # sensitivity to error
+
+for iter in range(5): # train by going through iterations
+    g = i1*w1 + i2*w2 # calculate "guess" from input and weights
+    e = abs(a-g) # error
+    
+    d = s*(a-g)/abs(a-g) # direction to move "guess" closer to "answer"
+    
+    w1 = w1 + d # update weight
+    w2 = w2 + d # update weight
+    print(g, e, w1, w2, d) # print out to let us see what's happening
+
+print g # output final "guess"
--- neuralNet2.py
+++ neuralNet2.py
@@ -1,15 +1,20 @@
 # i=input, w=weight, g=guess, a=answer, e=error, d=direction to move g closer to a, s=sensitivity
-i1 = 0
-i2 = 1
-w1 = -1
-w2 = -1
-a = 1
-for iter in range(5):
-    g = i1*w1 + i2*w2
-    e = (a-g)
-    dw1 = e*i1
-    dw2 = e*i2
-    w1 = w1 + dw1
-    w2 = w2 + dw2
-    print g, e, w1, w2, dw1, dw2
-print g
+
+i1 = 0 # input 1
+i2 = 1 # input 2
+w1 = -1 # weight 1
+w2 = -1 # weight 2
+a = 1 # the correct "answer"
+
+for iter in range(5): # train by going through iterations
+    g = i1*w1 + i2*w2 # calculate "guess" from input and weights
+    e = (a-g) # error
+    
+    dw1 = e*i1 # calculate change in weight 1 based on output error and input 1
+    dw2 = e*i2 # calculate change in weight 2 based on output error and input 2
+    
+    w1 = w1 + dw1 # update weight
+    w2 = w2 + dw2 # update weight
+    print(g, e, w1, w2, dw1, dw2) # print out to let us see what's happening
+
+print g # output final "guess"
