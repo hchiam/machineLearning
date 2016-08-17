07a21db2898aa5800b2ea193da5b3f82ae56c26e
hchiam <hchiam@gmail.com>
2016-08-17 13:50:21 -0400

    udpate loop & print

--- neuralNet1.py
+++ neuralNet1.py
@@ -8,14 +8,15 @@ a = 1 # the correct "answer"
 
 s = 0.1 # sensitivity to error
 
-for iter in range(5): # train by going through iterations
+for iter in range(21): # train by going through iterations
     g = i1*w1 + i2*w2 # calculate "guess" from input and weights
+    
     e = abs(a-g) # error
     
-    d = s*(a-g)/abs(a-g) # direction to move "guess" closer to "answer"
+    d = (a-g)/abs(a-g) # direction to move "guess" closer to "answer"
     
-    w1 = w1 + d # update weight
-    w2 = w2 + d # update weight
-    print(g, e, w1, w2, d) # print out to let us see what's happening
+    w1 = w1 + d*s # update weight
+    w2 = w2 + d*s # update weight
+    print g, e, w1, w2, d*s # print out to let us see what's happening
 
 print g # output final "guess"
--- neuralNet2.py
+++ neuralNet2.py
@@ -6,8 +6,9 @@ w1 = -1 # weight 1
 w2 = -1 # weight 2
 a = 1 # the correct "answer"
 
-for iter in range(5): # train by going through iterations
+for iter in range(3): # train by going through iterations
     g = i1*w1 + i2*w2 # calculate "guess" from input and weights
+    
     e = (a-g) # error
     
     dw1 = e*i1 # calculate change in weight 1 based on output error and input 1
@@ -15,6 +16,6 @@ for iter in range(5): # train by going through iterations
     
     w1 = w1 + dw1 # update weight
     w2 = w2 + dw2 # update weight
-    print(g, e, w1, w2, dw1, dw2) # print out to let us see what's happening
+    print g, e, w1, w2, dw1, dw2 # print out to let us see what's happening
 
 print g # output final "guess"
