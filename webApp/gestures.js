aff01d5c63667da8a307c6ffc464701eb55a08c4
hchiam <hchiam@gmail.com>
2016-09-26 00:33:45 -0400

    code reorganization for readability

--- webApp/gestures.js
+++ webApp/gestures.js
@@ -1,41 +1,26 @@
-function mouseMovingOverPad(event) {
+var sampleTimer;
+
+function mouseMovingOverPad(event) { // I'd recommend you read the code starting from here
     showCoords(event);
     var learn = toggleLearn();
-    learnGesture(learn);
+    learnGesture(learn, event);
     if (learn == false) {
         var gesture = detectGesture();
         showGesture(gesture);
     }
 }
 
-function clearDetections() {
-    document.getElementById("coords").innerHTML = "";
-    document.getElementById("gesture").innerHTML = "";
-}
-
 function showCoords(event) {
-    var x = event.clientX;
-    var y = event.clientY;
+    var x = getCoords(event)[0];
+    var y = getCoords(event)[1];
     var coords = "(" + x + "," + y + ")";
     document.getElementById("coords").innerHTML = coords;
 }
 
-function showGesture(gesture) {
-    document.getElementById("gesture").innerHTML = "Gesture: " + gesture + "?";
-}
-
-function toggleCheckboxText() {
-    if (document.getElementById("learn").checked == true) {
-        document.getElementById("yesno").innerHTML = " Learn gesture (ON)";
-    } else {
-        document.getElementById("yesno").innerHTML = " Learn gesture (off)";
-    }
-}
-
-function detectGesture() {
-    var gesture = "";
-    // goes here:  have ML algorithm try to categorize as gesture or not
-    return gesture;
+function getCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    return [x,y];
 }
 
 function toggleLearn() {
@@ -46,8 +31,40 @@ function toggleLearn() {
     return learn;
 }
 
-function learnGesture(learn) {
+function learnGesture(learn, event) {
     if (learn == true) {
         // goes here:  show ML algorithm an example gesture
+        sampleTimer = setInterval(getSamples(event), 200);
     }
+}
+
+function getSamples(event) {
+    getCoords(event);
+    // more stuff here
+}
+
+function detectGesture() {
+    var gesture = "";
+    // goes here:  have ML algorithm try to categorize as gesture or not
+    return gesture;
+}
+
+function showGesture(gesture) {
+    document.getElementById("gesture").innerHTML = "Gesture: " + gesture + "?";
+}
+
+// other functions based on other non-synchronous actions:
+
+function toggleCheckboxText() {
+    if (document.getElementById("learn").checked == true) {
+        document.getElementById("yesno").innerHTML = " Learn gesture (ON)";
+    } else {
+        document.getElementById("yesno").innerHTML = " Learn gesture (off)";
+    }
+}
+
+function clearDetections() { // "end" with this function
+    document.getElementById("coords").innerHTML = "";
+    document.getElementById("gesture").innerHTML = "";
+    document.getElementById("learn").checked = false; // auto stop learning when mouse leaves pad
 }
