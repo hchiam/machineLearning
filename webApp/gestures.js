d11ca0bf52f08757d010753bfa03c324e93ce634
hchiam <hchiam@gmail.com>
2016-09-28 15:08:01 -0400

    prelim neural net output. need to fix wts algo

--- webApp/gestures.js
+++ webApp/gestures.js
@@ -3,7 +3,7 @@ var padWidth = 100;
 var padHeight = 100;
 var shiftx = -9;
 var shifty = -9;
-var snapshots = 3;
+var snapshots = 50;
 var rows = 3;
 var columns = 3;
 var neuralNet = create3DMatrix(snapshots,rows,columns);
@@ -12,6 +12,7 @@ var yNN = rows;
 var zNN = snapshots;
 var numOfWts = xNN * yNN * zNN;
 var wts = create3DMatrix(snapshots,rows,columns); // so guarantee same size
+var testInputMatrix = create3DMatrix(snapshots,rows,columns); // so guarantee same size
 
 function create3DMatrix(snapshots,rows,columns) {
     var x = snapshots;
@@ -33,9 +34,14 @@ function create3DMatrix(snapshots,rows,columns) {
 function mouseMovingOverPad(event) { // I'd recommend you read the code starting from here
     showCoords(event);
     var learn = toggleLearn();
-    learnGesture(learn, event);
-    if (learn == false) {
-        var gesture = detectGesture();
+    if (learn == true) {
+        learnGesture(event);
+    } else if (learn == false) {
+        // stop learning
+        clearTimeout(sampleTimer);
+        updateSynapsesWeights(); // have ML algorithm set neuron synapse weights
+        // start detecting
+        var gesture = detectGesture(event);
         showGesture(gesture);
     }
 }
@@ -61,26 +67,24 @@ function toggleLearn() {
     return learn;
 }
 
-function learnGesture(learn, event) {
-    if (learn == true) {
-        sampleTimer = setInterval(updateNeuralNetwork(event), 1000*2); // 1 per 2 seconds = 1000*2 ; 10 per second = 1000/10
-    }
+function learnGesture(event) {
+    sampleTimer = setInterval(updateNeuralNetwork(event, neuralNet), 1000*2); // 1 per 2 seconds = 1000*2 ; 10 per second = 1000/10
 }
 
-function updateNeuralNetwork(event) {
-    shiftSamples(event); // show ML algorithm an example gesture
-    updateSynapsesWeights(); // have ML algorithm set neuron synapse weights
+function updateNeuralNetwork(event, matrix) {
+    shiftSamples(event, matrix); // show ML algorithm an example gesture
+    //updateSynapsesWeights(); // have ML algorithm set neuron synapse weights
 }
 
-function shiftSamples(event) {
+function shiftSamples(event, matrix) {
     var inputSectionMatrix = getPadSection(event);
-    var numberOfSnapshots = neuralNet.length;
+    var numberOfSnapshots = matrix.length;
     for (i = numberOfSnapshots-1; i > 0; i--) {
-        neuralNet[i] = neuralNet[i-1];
+        matrix[i] = matrix[i-1];
     }
-    neuralNet[0] = inputSectionMatrix; // example:  [[0,0,0],[0,0,0],[1,0,0]]
+    matrix[0] = inputSectionMatrix; // example:  [[0,0,0],[0,0,0],[1,0,0]]
     // debug output:
-    document.getElementById("section").innerHTML = neuralNet;
+    document.getElementById("section").innerHTML = matrix;
 }
 
 function getPadSection(event) {
@@ -144,10 +148,35 @@ function sigmoid(x) { // to keep number range within 0 to 1
     // "-x*6" because want to compress plot to have input ranging from 0 to 1 (and not 0 to 6)
 }
 
-function detectGesture() {
+function detectGesture(event) {
     var gesture = "";
-    // goes here:  have ML algorithm try to categorize as gesture or not
+    var outputValue = 0;
+    var x = zNN;
+    var y = yNN;
+    var z = xNN;
+    // track the input gesture
+    trackGesture(event);
+    // have ML algorithm try to categorize as gesture or not
+    for (i = 0; i < x; i++) {
+        for (j = 0; j < y; j++) {
+            for(k = 0; k < z; k++) {
+                outputValue += wts[i][j][k] * testInputMatrix[i][j][k];
+                outputValue = round( sigmoid( outputValue ) );
+            }
+        }
+    }
+    // debug output
+    document.getElementById("confidence").innerHTML = "confidence"+outputValue;
+    if (outputValue > 0.8) {
+        gesture = "Gesture A";
+    } else {
+        gesture = "";
+    }
     return gesture;
+}
+
+function trackGesture(event) {
+    sampleTimer = setInterval(updateNeuralNetwork(event, testInputMatrix), 1000*2); // 1 per 2 seconds = 1000*2 ; 10 per second = 1000/10
 }
 
 function showGesture(gesture) {
