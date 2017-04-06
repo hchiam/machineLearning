e18442e1b86d66f72e0621d6d85f90ad2d946a87
hchiam <hchiam@users.noreply.github.com>
2017-04-06 00:15:24 -0400

    add comments and space out

--- googleExamples/googleExample.py
+++ googleExamples/googleExample.py
@@ -1,6 +1,19 @@
-from sklearn import tree # Watch source video for explanation:  https://youtu.be/cKxRvEZd3Mw in the Google Developers channel
+# Watch source video for explanation: https://youtu.be/cKxRvEZd3Mw in the Google Developers channel
+
+# IMPORT TREE module from scikit-learn
+from sklearn import tree
+
+# get DATA set of features corresponding to each label
 features = [[140, 1], [130, 1], [150, 0], [170, 0]] # weights and bumpiness for each example
+
+# get LABELS (with indices corresponding to the respective features indices)
 labels = [0, 0, 1, 1] # labels for each example
-clf = tree.DecisionTreeClassifier() # create a decision tree classifier
+
+# create a decision tree CLASSIFIER
+clf = tree.DecisionTreeClassifier()
+
+# TRAIN the classifier
 clf = clf.fit(features, labels) # fit features and labels; "find patterns in the data"
+
+# print OUTPUT
 print clf.predict([[160, 0]]) # 0 = apple ; 1 = orange
