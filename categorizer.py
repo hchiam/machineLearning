18816b176f53479b3ac2adf2372c8bf679f65452
hchiam <hchiam@gmail.com>
2017-04-10 03:34:48 -0400

    example categorizer reading from csv file

---
+++ categorizer.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+f = open("data.csv")
+# f.readline() # skip the first row
+data = np.loadtxt(f)
+
+features = data[:,[0,1,2,3]]
+labels = data[:,4]
+
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print(clf.predict([[160, 0, 1, 2]]))
