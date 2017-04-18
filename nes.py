2b4dce2c4f52af5b94d134ae4fa35a843409a04a
hchiam <hchiam@gmail.com>
2017-04-18 10:51:39 -0400

    make indents consistent

--- nes.py
+++ nes.py
@@ -15,7 +15,7 @@ def f(w):
   # ... 1) create a neural network with weights w
   # ... 2) run the neural network on the environment for some time
   # ... 3) sum up and return the total reward
-
+  
   # but for the purposes of an example, lets try to minimize
   # the L2 distance to a specific solution vector. So the highest reward
   # we can achieve is 0, when the vector w is exactly equal to solution
@@ -31,19 +31,19 @@ alpha = 0.001 # learning rate
 solution = np.array([0.5, 0.1, -0.3])
 w = np.random.randn(3) # our initial guess is random
 for i in range(300):
-
+  
   # print current fitness of the most likely parameter setting
   if i % 20 == 0:
     print('iter %d. w: %s, solution: %s, reward: %f' % 
           (i, str(w), str(solution), f(w)))
-
+  
   # initialize memory for a population of w's, and their rewards
   N = np.random.randn(npop, 3) # samples from a normal distribution N(0,1)
   R = np.zeros(npop)
   for j in range(npop):
     w_try = w + sigma*N[j] # jitter w using gaussian of sigma 0.1
     R[j] = f(w_try) # evaluate the jittered version
-
+  
   # standardize the rewards to have a gaussian distribution
   A = (R - np.mean(R)) / np.std(R)
   # perform the parameter update. The matrix multiply below
